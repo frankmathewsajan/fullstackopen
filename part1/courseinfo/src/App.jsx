acb28b417edd19d669ca2fd544f652ff84f7e0a0
frankmathewsajan <145902617+frankmathewsajan@users.noreply.github.com>
2025-02-05 01:21:07 +0530

    finished 1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -15,9 +15,9 @@ const Part = (props) => {
 const Content = (props) => {
     return (
         <>
-            <Part name={props.names[0]} count={props.count[0]}/>
-            <Part name={props.names[1]} count={props.count[1]}/>
-            <Part name={props.names[2]} count={props.count[2]}/>
+            <Part name={props.values[0]['name']} count={props.values[0]['exercises']}/>
+            <Part name={props.values[1]['name']} count={props.values[1]['exercises']}/>
+            <Part name={props.values[2]['name']} count={props.values[2]['exercises']}/>
         </>
     )
 }
@@ -29,18 +29,24 @@ const Total = (props) => {
 }
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
-
+    const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
+    }
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+    }
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    }
+    let total = part1['exercises'] + part2['exercises'] + part3['exercises']
     return (
         <div>
             <Header course={course}/>
-            <Content names={[part1, part2, part3]} count={[exercises1, exercises2, exercises3]}/>
-            <Total total={exercises1 + exercises2 + exercises3}/>
+            <Content values={[part1, part2, part3]} />
+            <Total total={total}/>
         </div>
     )
 }
