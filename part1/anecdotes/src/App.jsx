64ee9a2c8a4caefec14a62ccdf5e509ac20cabee
frankmathewsajan <145902617+frankmathewsajan@users.noreply.github.com>
2025-02-07 13:16:53 +0530

    added Math.random

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import {useState} from "react";
 
-function NextButton({ onClick }) {
+function NextButton({onClick}) {
     return <button onClick={onClick}>next</button>;
 }
 
-function VoteButton({ onClick }) {
+function VoteButton({onClick}) {
     return <button onClick={onClick}>vote</button>;
 }
 
@@ -20,7 +20,7 @@ const App = () => {
         "The only way to go fast, is to go well."
     ];
 
-    const [selected, setSelected] = useState(0);
+    const [selected, setSelected] = useState(Math.floor(Math.random() * (8)));
     const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
     const [highest, setHighest] = useState(0);
 
@@ -45,18 +45,18 @@ const App = () => {
         <>
             <h1>Anecdote of the day</h1>
             <div>
-                {anecdotes[selected]} <br />
-                <br />
+                {anecdotes[selected]} <br/>
+                <br/>
                 has {votes[selected]} {votes[selected] === 1 ? "vote" : "votes"}
             </div>
-            <br />
-            <VoteButton onClick={handleVote} />
-            <NextButton onClick={handleNext} />
+            <br/>
+            <VoteButton onClick={handleVote}/>
+            <NextButton onClick={handleNext}/>
 
             <h1>Anecdote with most votes</h1>
             <div>
-                {anecdotes[highest]} <br />
-                <br />
+                {anecdotes[highest]} <br/>
+                <br/>
                 has {votes[highest]} {votes[highest] === 1 ? "vote" : "votes"}
             </div>
         </>
