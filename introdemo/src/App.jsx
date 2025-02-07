ad1ce34d4a409f3cbc61735a734035372df130fd
frankmathewsajan <145902617+frankmathewsajan@users.noreply.github.com>
2025-02-07 10:50:10 +0530

    ex1.6 - 1.14

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,10 +1,40 @@
-const App = () => {
-  const friends = [ 'Peter', 'Maya']
+import {useState} from "react";
+// UseState re-renders the whole thing, so don't use it directly.
+// ie. Calling a function that changes the state causes the component to re-render.
+// lift the state up in the component hierarchy.
+// Do not mutate state directly
+// Since state changes are asyncronous, put the new values in a `const` first
 
+const Display = ({counter}) => {
   return (
-    <div>
-      <p>{friends}</p>
-    </div>
+      <div>{counter}</div>
   )
 }
+
+const Button = ({onClick,text}) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+const App = () => {
+    const [counter, setCounter] = useState(0)
+
+
+    const increaseByOne = () => setCounter(counter + 1)
+
+    const setToZero = () => setCounter(0)
+
+    return (
+        <>
+            <Display counter={counter}/>
+
+            <Button onClick={increaseByOne} text="+"/>
+
+            <Button onClick={setToZero} text="to 0"/>
+        </>
+    )
+}
 export default App
+
