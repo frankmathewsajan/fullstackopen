d2470fbe3c4caa2be0b7937d6a271483c4ca99dd
frankmathewsajan <145902617+frankmathewsajan@users.noreply.github.com>
2025-02-09 14:19:20 +0530

    notes updated with json-server and axios

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,10 +1,17 @@
 import Note from './components/Note'
-import {useState} from "react";
+import {useState, useEffect} from "react";
+import axios from "axios";
 
-const App = (props) => {
-    const [notes, setNotes] = useState(props.notes)
+const App = () => {
+    const [notes, setNotes] = useState([])
     const [newNote, setNewNote] = useState('')
     const [showAll, setShowAll] = useState(true)
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/notes').then((res) => {
+            setNotes(res.data)
+        })
+    }, [])
     const handleSubmit = (event) => {
         event.preventDefault()
         if (newNote === '') return false
@@ -21,13 +28,13 @@ const App = (props) => {
         setNewNote(target.value)
     }
     const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important === true)
+        ? notes
+        : notes.filter(note => note.important === true)
     return (
         <div>
             <h1>Notes</h1>
-            <button onClick={()=>setShowAll(!showAll)}>
-                show {showAll? 'important' : 'all'}
+            <button onClick={() => setShowAll(!showAll)}>
+                show {showAll ? 'important' : 'all'}
             </button>
             <ul>
                 {notesToShow.map(note =>
