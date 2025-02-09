4a6dd99bb599151559db8199b100daa650bc4cb4
frankmathewsajan <145902617+frankmathewsajan@users.noreply.github.com>
2025-02-09 15:44:29 +0530

    added axios and separated it's logic

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,15 +1,27 @@
 import Note from './components/Note'
 import {useState, useEffect} from "react";
-import axios from "axios";
+import noteService from './services/notes.js'
 
 const App = () => {
     const [notes, setNotes] = useState([])
     const [newNote, setNewNote] = useState('')
     const [showAll, setShowAll] = useState(true)
 
+    const toggleImportanceOf = id => {
+        const note = notes.find(n => n.id === id)
+        const changedNote = {...note, important: !note.important}
+
+        noteService.update(id, changedNote).then(response => {
+            setNotes(notes.map(n => n.id === id ? response : n))
+        }).catch(() => {
+            alert(`the note '${note.content}' was already deleted from server`)
+            setNotes(notes.filter(n => n.id !== id))
+        })
+    }
+
     useEffect(() => {
-        axios.get('http://localhost:3001/notes').then((res) => {
-            setNotes(res.data)
+        noteService.getAll().then((res) => {
+            setNotes(res)
         })
     }, [])
     const handleSubmit = (event) => {
@@ -20,9 +32,10 @@ const App = () => {
             important: Math.random() < 0.5,
             id: String(notes.length + 1),
         }
-
-        setNotes(notes.concat(noteObject))
-        setNewNote('')
+        noteService.create(noteObject).then(res => {
+            setNotes(notes.concat(res))
+            setNewNote('')
+        })
     }
     const handleNoteChange = ({target}) => {
         setNewNote(target.value)
@@ -38,7 +51,9 @@ const App = () => {
             </button>
             <ul>
                 {notesToShow.map(note =>
-                    <Note key={note.id} note={note}/>
+                    <Note key={note.id} note={note} toggleImportance={() => {
+                        toggleImportanceOf(note.id)
+                    }}/>
                 )}
             </ul>
             <form onSubmit={handleSubmit}>
--- part2/notes/src/components/Note.jsx
+++ part2/notes/src/components/Note.jsx
@@ -1,7 +1,11 @@
-const Note = ({ note }) => {
-  return (
-    <li>{note.content}</li>
-  )
+const Note = ({note, toggleImportance}) => {
+    return (
+        <>
+            <li>{note.content}</li>
+            <button onClick={toggleImportance}>{note.important ? 'not important' : 'important'}
+            </button>
+        </>
+    )
 }
 
 export default Note
