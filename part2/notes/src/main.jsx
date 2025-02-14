7608d61c8439cebba62b83028351ceb66bd9e8a1
frankmathewsajan <145902617+frankmathewsajan@users.noreply.github.com>
2025-02-14 12:08:30 +0530

    conditional host

--- part2/notes/src/main.jsx
+++ part2/notes/src/main.jsx
@@ -2,9 +2,10 @@ import ReactDOM from 'react-dom/client'
 import axios from "axios";
 import './index.css'
 
-
+const useAWS = true
+const host = useAWS ? `51.21.181.130` : '127.0.0.1'
 axios
-    .get('http://localhost:3001/api/notes')
+    .get(`http://${host}:3001/api/notes`)
     .then((res) => {
         console.log(res.data)
     })
